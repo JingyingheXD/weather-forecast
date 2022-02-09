6fefe1e70840df520ad1094eb2ea96b75f0888b5
Jingying He <450883724@qq.com>
2022-02-09 22:20:15 +1100

    layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import "./App.css";
+import WeatherDetails from "./components/weather-details";
+import Footer from "./components/footer";
 
 function App() {
   useEffect(() => {
     fetch(
-      "https://api.openweathermap.org/data/2.5/onecall?lat=-37&lon=145&exclude=daily&appid=e1fb6136198f0c0a8cd978f199e1a78a",
+      "https://api.openweathermap.org/data/2.5/onecall?lat=-37&lon=145&appid=e1fb6136198f0c0a8cd978f199e1a78a",
       { method: "GET" }
     )
       .then((resp) => resp.json())
@@ -13,7 +15,12 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">Weather Forecast</header>
+      <div className="card">
+        <WeatherDetails />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/weather-details.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WeatherDetails(props) {
+  return <div>Weather details</div>;
+}
+
+export default WeatherDetails;
