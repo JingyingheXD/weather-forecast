5af3c91582163d18dfb7464308e54d1ae7f86f1d
Jingying He <450883724@qq.com>
2022-02-10 12:36:54 +1100

    convert UTC timestamp format

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,32 @@
 import React from "react";
 
 function Footer(props) {
+  const dateTime = (UnixTimeStamp) => {
+    let a = new Date(UnixTimeStamp * 1000);
+    var months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var time = date + " " + month;
+    return time;
+  };
+
   return (
     <div>
       <h2>{props.dailyWeather.weather[0].main}</h2>
-      <h2>{props.dailyWeather.dt}</h2>
+      <h2>{dateTime(props.dailyWeather.dt)}</h2>
     </div>
   );
 }
