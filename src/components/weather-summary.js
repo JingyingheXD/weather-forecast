43ffd02c9ca568a020e5d5d2b7b9003cacbd9487
Jingying He <450883724@qq.com>
2022-02-13 14:41:03 +1100

    restructure

--- src/components/weather-summary.js
+++ src/components/weather-summary.js
@@ -4,18 +4,24 @@ function WeatherSummary(props) {
   const currentWeather = props.currentWeather;
   const dailyWeathers = props.dailyWeathers;
 
+  const celsiusTemp = (tempKelvin) => {
+    var tempCelsius;
+    tempCelsius = Math.round(tempKelvin - 273.15);
+    return tempCelsius;
+  };
+
   return (
     currentWeather && (
       <div className="weather-sum">
         <div className="weather-sum-degree">
-          {ConvertKelvinToCelsius(currentWeather.temp)}&deg;
+          {celsiusTemp(currentWeather.temp)}&deg;
         </div>
         <div className="weather-sum-description">
           {props.currentWeather.weather[0].description}
         </div>
         <div className="weather-sum-Low-High">
-          L: {ConvertKelvinToCelsius(dailyWeathers[0].temp.min)}&deg; H:{" "}
-          {ConvertKelvinToCelsius(dailyWeathers[0].temp.max)}&deg;
+          L: {celsiusTemp(dailyWeathers[0].temp.min)}&deg; H:{" "}
+          {celsiusTemp(dailyWeathers[0].temp.max)}&deg;
         </div>
         <div className="weather-sum-button">
           <div className="d-flex justify-content-center">
