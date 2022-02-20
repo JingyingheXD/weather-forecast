43167713c9fd23aec6b674105d01503c23179006
Jingying He <450883724@qq.com>
2022-02-20 23:18:43 +1100

    update module layout

--- src/components/weather-details-module.js
+++ src/components/weather-details-module.js
@@ -3,8 +3,8 @@ import React from "react";
 function WeatherDetailsModule(props) {
   return (
     <div className="weather-details-module">
-      <h3>{props.itemName}</h3>
-      <div>{props.item}</div>
+      <div className="weather-details-module-title">{props.itemName}</div>
+      <div className="weather-details-module-content">{props.item}</div>
     </div>
   );
 }
