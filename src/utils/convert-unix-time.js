fc11aa6ec6c1c7ccbe10ecb9e6b02a77bfe3a26c
Jingying He <450883724@qq.com>
2022-02-23 21:29:56 +1100

    parse unix time with day.js

--- src/utils/convert-unix-time.js
+++ src/utils/convert-unix-time.js
@@ -1,7 +1,56 @@
+import dayjs from "dayjs";
+
+// function ConvertUnixTime(UnixTimeStamp, displayTime) {
+//   const dateTime = (UnixTimeStamp, displayTime) => {
+//     let timeNow = new Date(Date.now());
+//     let a = new Date(UnixTimeStamp * 1000);
+
+//     let months = [
+//       "Jan",
+//       "Feb",
+//       "Mar",
+//       "Apr",
+//       "May",
+//       "Jun",
+//       "Jul",
+//       "Aug",
+//       "Sep",
+//       "Oct",
+//       "Nov",
+//       "Dec",
+//     ];
+//     let month = months[a.getMonth()];
+//     let date = a.getDate();
+//     let hour = a.getHours();
+//     let min = a.getMinutes();
+//     if (hour < 10){
+
+//     }
+//     let outputDate = date + " " + month;
+//     let outputTime = hour + ":" + min;
+//     if (displayTime == 0) {
+//       if (
+//         timeNow.getDate() == a.getDate() &&
+//         timeNow.getMonth() == timeNow.getMonth()
+//       ) {
+//         return "Today";
+//       } else {
+//         return outputDate;
+//       }
+//     } else {
+//       return outputTime;
+//     }
+//   };
+
+//   let newTime = dateTime(UnixTimeStamp, displayTime);
+
+//   return newTime;
+// }
+
 function ConvertUnixTime(UnixTimeStamp, displayTime) {
   const dateTime = (UnixTimeStamp, displayTime) => {
-    let timeNow = new Date(Date.now());
-    let a = new Date(UnixTimeStamp * 1000);
+    let timeNow = dayjs();
+    let a = dayjs.unix(UnixTimeStamp);
 
     let months = [
       "Jan",
@@ -17,20 +66,17 @@ function ConvertUnixTime(UnixTimeStamp, displayTime) {
       "Nov",
       "Dec",
     ];
-    let month = months[a.getMonth()];
-    let date = a.getDate();
-    let hour = a.getHours();
-    let min = a.getMinutes();
-    if (hour < 10){
-      
-    }
-    let outputDate = date + " " + month;
-    let outputTime = hour + ":" + min;
+
+    let month = a.month();
+    let date = a.date();
+    let hour = a.hour();
+    let minute = a.minute();
+
+    let outputDate = date + " " + months[month];
+    let outputTime = hour + " " + minute;
+
     if (displayTime == 0) {
-      if (
-        timeNow.getDate() == a.getDate() &&
-        timeNow.getMonth() == timeNow.getMonth()
-      ) {
+      if (timeNow.date() == date && timeNow.month == month) {
         return "Today";
       } else {
         return outputDate;
@@ -40,9 +86,9 @@ function ConvertUnixTime(UnixTimeStamp, displayTime) {
     }
   };
 
-  let newTime = dateTime(UnixTimeStamp, displayTime);
+  let output = dateTime(UnixTimeStamp, displayTime);
 
-  return newTime;
+  return output;
 }
 
 export default ConvertUnixTime;
