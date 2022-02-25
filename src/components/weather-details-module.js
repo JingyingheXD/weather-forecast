3ca184d000832f53fd979f3a6c91c3630fe57d0c
Jingying He <450883724@qq.com>
2022-02-25 14:12:22 +1100

    update layout

--- src/components/weather-details-module.js
+++ src/components/weather-details-module.js
@@ -3,8 +3,10 @@ import React from "react";
 function WeatherDetailsModule(props) {
   return (
     <div className="weather-details-module">
-      <div className="weather-details-module-title">{props.itemName}</div>
-      <div className="weather-details-module-content">{props.item}</div>
+      <div className="d-flex align-items-center mx-auto weather-details-module-title">
+        {props.itemName}
+      </div>
+      <div className="mx-auto weather-details-module-content">{props.item}</div>
     </div>
   );
 }
